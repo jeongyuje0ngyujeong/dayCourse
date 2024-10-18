03f0f5017e063ab0df041b4d9b51406bd517c716
aehyemin <hsy13467926@gmail.com>
2024-10-18 13:42:53 +0900

    style com

--- dayCourse_client/client/src/pages/Elbum/Elbum.js
+++ dayCourse_client/client/src/pages/Elbum/Elbum.js
@@ -1,9 +1,18 @@
 // import { Outlet} from "react-router-dom";
+import styled from 'styled-components';
+
+import React from 'react';
+import Search from './Search.js';
+import Moment from './moment.js';
 
 export default function Elbum() {
   return (
     <>
-    <div>elbum</div>
+    <div>
+      <Search/>
+      <Moment/>
+      ㅓ허허ㅗ허ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ
+    </div>
     </>
   );
 }
---
+++ dayCourse_client/client/src/pages/Elbum/moment.js
@@ -0,0 +1,16 @@
+
+import React, {useState} from 'react';
+
+
+
+const Moment = () => {
+
+    return (
+        <div>
+            <h1>모먼트</h1>
+            
+        </div>
+    );
+};
+
+export default Moment;
