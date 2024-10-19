08ac67929730aff4146e8f002885b920f52b2d15
jeongyuje0ngyujeong <jeongyuje0ngyujeong.j@gmail.com>
2024-10-19 15:12:27 +0900

    search blog api success, data lab search api in progress

---
+++ dayCourse_server/test/search-blog.js
@@ -0,0 +1,33 @@
+// 네이버 검색 API 예제 - 블로그 검색
+var express = require('express');
+var axios = require('axios');
+var app = express();
+
+var client_id = 'mJquI2shJRRNjhQiXfrN';
+var client_secret = 'YtFz32RaxB';
+
+
+app.get('/search/blog', async function (req, res) {
+    const query = '광교 오늘의초밥';
+    const encodedQuery = encodeURIComponent(query);
+    const api_url = `https://openapi.naver.com/v1/search/blog.json?query=${encodedQuery}`;
+
+    try {
+        const response = await axios.get(api_url, {
+            headers: {
+                'X-Naver-Client-Id': client_id,
+                'X-Naver-Client-Secret': client_secret
+            }
+        });
+        
+        res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+        res.end(JSON.stringify(response.data));
+    } catch (error) {
+        res.status(error.response ? error.response.status : 500).end();
+        console.log('error = ' + (error.response ? error.response.status : error.message));
+    }
+});
+
+app.listen(3000, function () {
+   console.log('http://127.0.0.1:3000/search/blog?query=검색어 app listening on port 3000!');
+});
