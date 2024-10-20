1a6b4e5d12e77e2971896f20c98f7eedd384b2bd
jeongyuje0ngyujeong <jeongyuje0ngyujeong.j@gmail.com>
2024-10-21 01:00:58 +0900

    git action setting
    git action setting

--- dayCourse_server/test/search-blog.js
+++ dayCourse_server/test/search-blog.js
@@ -28,6 +28,27 @@ app.get('/search/local', async function (req, res) {
     }
 });
 
-app.listen(3000, function () {
-   console.log('http://127.0.0.1:3000/search/local?query=검색어 app listening on port 3000!');
+app.get('/search/local3', async function (req, res) {
+    const query = '연신내';
+    const encodedQuery = encodeURIComponent(query);
+    const api_url = `https://openapi.naver.com/v1/search/local.json?query=${encodedQuery}`;
+
+    try {
+        const response = await axios.get(api_url, {
+            headers: {
+                'X-Naver-Client-Id': client_id,
+                'X-Naver-Client-Secret': client_secret
+            }
+        });
+        
+        res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+        res.end(JSON.stringify(response.data));
+    } catch (error) {
+        res.status(error.response ? error.response.status : 500).end();
+        console.log('error = ' + (error.response ? error.response.status : error.message));
+    }
+});
+
+app.listen(80, function () {
+   console.log('http://127.0.0.1:80/search/local?query=검색어 app listening on port 80!!');
 });
