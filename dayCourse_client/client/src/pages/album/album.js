6d43b2278ad2c2018522f3af18bfd9d9e53f5452
jeongyuje0ngyujeong <jeongyuje0ngyujeong.j@gmail.com>
2024-10-21 23:57:19 +0900

    deploy test

--- dayCourse_client/client/src/pages/album/album.js
+++ dayCourse_client/client/src/pages/album/album.js
@@ -1,6 +1,5 @@
 // import { Outlet} from "react-router-dom";
 import styled from 'styled-components';
-
 import React from 'react';
 import Search from './Search.js';
 import Moment from './moment.js';
