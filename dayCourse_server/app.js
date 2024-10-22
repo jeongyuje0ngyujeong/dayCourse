0f5f89ec38131bdf87ad52453283f997acc2d67f
jeongyuje0ngyujeong <jeongyuje0ngyujeong.j@gmail.com>
2024-10-22 14:05:40 +0900

    test

--- dayCourse_server/app.js
+++ dayCourse_server/app.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const requestListener = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
