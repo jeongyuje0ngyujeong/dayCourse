c1c6b864c302acd89316533ac5b1036384d66924
jeongyuje0ngyujeong <jeongyuje0ngyujeong.j@gmail.com>
2024-10-22 13:46:56 +0900

    test

---
+++ dayCourse_server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+// server.js
+const PORT = 5000;
+
+const requestListener = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    if (req.method === 'GET' && req.url === '/home') {
+        res.writeHead(200);
+        res.end(JSON.stringify({ message: '서버와 클라이언트 통신이 성공했습니다!' }));
+    } else {
+        res.writeHead(404);
+        res.end(JSON.stringify({ message: '찾을 수 없는 페이지입니다.' }));
+    }
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT, () => {
+    console.log(`서버가 ${PORT}에서 실행 중입니다.`);
+});
