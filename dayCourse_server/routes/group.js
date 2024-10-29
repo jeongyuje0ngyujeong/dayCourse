4431e2dc63f2544ce658969215fe02ae59f5c5ff
jeongyuje0ngyujeong <jeongyuje0ngyujeong.j@gmail.com>
2024-10-29 16:15:48 +0900

    friend add

--- dayCourse_server/routes/group.js
+++ dayCourse_server/routes/group.js
@@ -5,6 +5,51 @@ const axios = require('axios');
 
 const authenticateJWT = require('../config/authenticateJWT');
 
+// 친구 찾기
+router.post('/friend', authenticateJWT, async (req, res) => {
+    console.log("req.user:", JSON.stringify(req.user, null, 2));
+    
+    const userId = req.user.userId;
+    console.log('usderId: ' + userId);
+    
+    const { searchId } = req.body;
+    
+    console.log('친구 검색')
+    
+    // Check if required parameters are provided
+    if (!searchId) {
+        return res.status(400).json({ error: 'searchId is required' });
+    }
+    
+    const find_sql = `
+    SELECT User.userId, User.userName
+    FROM User
+    WHERE User.id = ?
+    `;
+    
+    db.query(find_sql, [searchId], (err, find_result) => {
+        if (err) {
+            console.error('Error inserting data:', err);
+            return res.status(500).json({ error: 'Database error' });
+        }
+        
+        if (find_result.length > 0) {
+            console.log("find friend", JSON.stringify(find_result, null, 2));
+            
+            const friendUserId = find_result[0].userId;
+            const friendUserName = find_result[0].userName;
+            console.log('찾은 친구의 userId: ' + friendUserId);
+            
+            console.log("친구 찾기 성공!")
+            return res.status(201).json({ success: true, friendName: friendUserName });
+            
+        } else {
+            return res.status(404).json({ success: false, error: 'No user found with the provided searchId' });
+        }
+    });
+});
+
+// 친구 추가
 router.post('/friend/add', authenticateJWT, async (req, res) => {
     const userId = req.user.userId;
     console.log('usderId: ' + userId);
@@ -20,7 +65,7 @@ router.post('/friend/add', authenticateJWT, async (req, res) => {
     }
 
     const find_sql = `
-      SELECT User.userId
+      SELECT User.userId, User.id, User.userName
       FROM User
       WHERE User.id = ?
     `;
@@ -34,15 +79,17 @@ router.post('/friend/add', authenticateJWT, async (req, res) => {
         if (find_result.length > 0) {
             console.log("find friend: ", JSON.stringify(find_result, null, 2));
             const friendUserId = find_result[0].userId;
+            const friendId = find_result[0].id;
+            const friendName = find_result[0].userName;
 
             console.log("friendUserId: ", friendUserId);
 
             const inser_sql = `
-                INSERT IGNORE INTO friend (userId, friendUserId)
-                VALUES (?, ?)
+                INSERT IGNORE INTO friend (userId, friendUserId, friendName, friendId)
+                VALUES (?, ?, ?, ?)
             `;
             
-            const values = [userId, friendUserId];
+            const values = [userId, friendUserId, friendName, friendId];
 
             db.query(inser_sql, values, (err, insert_result) => {
                 console.log("insert_result: ", JSON.stringify(insert_result, null, 2));
@@ -66,47 +113,7 @@ router.post('/friend/add', authenticateJWT, async (req, res) => {
     });
 });
 
-router.post('/friend', authenticateJWT, async (req, res) => {
-    console.log("req.user:", JSON.stringify(req.user, null, 2));
+// 친구 리스트
 
-    const userId = req.user.userId;
-    console.log('usderId: ' + userId);
-
-    const { searchId } = req.body;
-
-    console.log('친구 검색')
-
-    // Check if required parameters are provided
-    if (!searchId) {
-        return res.status(400).json({ error: 'searchId is required' });
-    }
-
-    const find_sql = `
-      SELECT User.userId, User.userName
-      FROM User
-      WHERE User.id = ?
-    `;
-
-    db.query(find_sql, [searchId], (err, find_result) => {
-        if (err) {
-            console.error('Error inserting data:', err);
-            return res.status(500).json({ error: 'Database error' });
-        }
-
-        if (find_result.length > 0) {
-            console.log("find friend", JSON.stringify(find_result, null, 2));
-
-            const friendUserId = find_result[0].userId;
-            const friendUserName = find_result[0].userName;
-            console.log('찾은 친구의 userId: ' + friendUserId);
-
-            console.log("친구 찾기 성공!")
-            return res.status(201).json({ success: true, friendName: friendUserName });
-
-        } else {
-            return res.status(404).json({ success: false, error: 'No user found with the provided searchId' });
-        }
-    });
-});
 
 module.exports = router;
