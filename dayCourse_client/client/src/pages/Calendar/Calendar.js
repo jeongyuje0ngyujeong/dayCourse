f13349dbc67b5825b5cfe11f7ae3f96371c0b127
Minkyoung00 <kina12345600@gmail.com>
2024-10-30 20:49:06 +0900

    add adding departure

--- dayCourse_client/client/src/pages/Calendar/Calendar.js
+++ dayCourse_client/client/src/pages/Calendar/Calendar.js
@@ -2,7 +2,7 @@ import { PageTitle, Footer } from '../../commonStyles';
 import { Button } from '../../Button';
 import {useState,useEffect } from 'react';
 import styled from 'styled-components';
-import { getSchedule} from "../../schedules";
+import { getSchedule, getSchedules} from "../../schedules";
 import Modal from 'react-modal';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Schedule from "./Schedule";
@@ -128,23 +128,24 @@ async function fetchSchedules(props, setSchedules) {
   let scheduleMap = {};
   let currentDate = new Date(props.startDay);
   
-  // const dateKey = `${currentDate.getFullYear()}${String(currentDate.getMonth() + 1).padStart(2,'0')}${String(currentDate.getDate()).padStart(2,'0')}`
-  // console.log(dateKey);
-  // let schedules = await getSchedules(null, dateKey);
+  const dateKey = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2,'0')}-${String(currentDate.getDate()).padStart(2,'0')}`
+  let schedules = await getSchedules(null, dateKey);
 
   while (currentDate <= props.endDay) {
-    const dateKey = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2,'0')}-${currentDate.getDate()}`;
+    // const dateKey = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2,'0')}-${currentDate.getDate()}`;
     const schedule = await getSchedule(dateKey);
     scheduleMap[dateKey] = schedule;
     currentDate.setDate(currentDate.getDate() + 1);
   }
   setSchedules(scheduleMap); 
+  console.log('fetchSchedules: ', schedules);
 }
 
 // 시작 날짜, 끝 날짜를 인자로 호출
 // 시작, 끝 날짜 바뀔 때 해당 기간 데이터 재요청 
 export function GroupDatesByWeek(props){
   const [schedules, setSchedules] = useState([]); 
+  console.log('GroupDatesByWeek: ',props)
   
   useEffect(() => {
     fetchSchedules(props, setSchedules); 
---
+++ dayCourse_client/client/src/pages/Town/SearchKeyword.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import {Button} from '../../Button';
+import { Form, redirect, } from "react-router-dom";
+import styled from "styled-components";
+
+const ResultContainer = styled.div`
+    position: absolute; 
+    top: 100%; 
+    left: 0; 
+    max-height: 30rem;  
+    width: 100%; 
+    background: white; 
+    border: 1px solid #eee; 
+    border-top: none;
+    z-index: 1000; 
+    overflow-y: auto; 
+`
+
+const StyledForm = styled(Form)`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    position: relative; 
+`;
+
+export default function SearchKeyword({keyword, setKeyword, places, setPlaces, departurePoints, setDeparturePoints}){
+    const [value, setValue] = useState(""); // 입력 값 상태
+    const [showResult, setShowResult] = useState(false); // 결과 표시 여부
+
+    const keywordChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    // 제출한 검색어 상태 업데이트
+    const submitKeywordForm = (e) => {
+        e.preventDefault();
+        setKeyword(value); // 제출한 검색어를 상태로 저장
+        setValue('');
+        setShowResult(true);
+    };
+
+    const addDeparturePoint = (place) => {
+        // 최대 10개의 출발지 입력창만 추가할 수 있게 제한
+        if (departurePoints.length < 10) {
+            setDeparturePoints([...departurePoints, place]);
+        }
+        else {
+            alert('출발지는 최대 10개 설정할 수 있어요')
+        }
+    };
+
+    const handleDepartureChange = (index, value) => {
+        const updatedPoints = [...departurePoints];
+        updatedPoints[index] = value;
+        setDeparturePoints(updatedPoints);
+    }
+
+    return(
+        <>
+        <h3 style={{marginTop: '1rem'}}>출발지</h3>
+        <StyledForm onSubmit={submitKeywordForm}>
+            <input
+                type="text"
+                placeholder={`출발지를 추가해보세요`}
+                value={value}
+                // onChange={(e) => handleDepartureChange(e.target.value)}
+                onChange={keywordChange}
+                onFocus={() => setShowResult(true)} // 포커스를 얻으면 결과 표시
+                onBlur={() => {setShowResult(false); setPlaces([]);}}
+                style={{ border: '1px solid #ccc', padding: '5px', width:'100%'}}
+            />
+            <Button type='submit'  width='3rem' height='100%'>검색</Button> 
+            {/* {departurePoints.length <= 10 && ( departurePoints.length !== 10 ?
+                :<Button disabled width='3rem' height='100%'>검색</Button>
+            )} */}
+            <ResultContainer>
+                {showResult && (
+                <>
+                {places.length === 0 && ( // 검색 결과가 없을 때
+                    <p className="result-text">검색 결과가 없습니다.</p>
+                )}
+                <ul id="places-list">
+                    {places.map((place, index) => (
+                        <li 
+                            key={index} 
+                            onClick={() => {
+                                addDeparturePoint(place);
+                                setPlaces([]);
+                                setShowResult(false);
+                            }} 
+                            style={{ cursor: 'pointer' }}
+                        >
+                            <h5>{place.place_name}</h5>
+                            {place.road_address_name && <span>{place.road_address_name}</span>}
+                            <span>{place.address_name}</span>
+                        </li>
+                    ))}
+                </ul>
+                </>
+                )}
+            </ResultContainer>
+        </StyledForm>
+        </>
+    )
+}
