fdeae52c934e0c11f4b48b133cd135e322ebbe80
kimkeongeun <keongeun01@gmail.com>
2024-10-30 15:07:48 +0900

    home 그룹 포함 변경

--- dayCourse_server/routes/home.js
+++ dayCourse_server/routes/home.js
@@ -74,6 +74,7 @@ router.get('/', authenticateJWT, async (req, res) => {
         };
 
         //결과 두개합치기
+        //테스트
         const formattedResult1 = formatResult(result1);
         const formattedResult2 = formatResult(result2);
         const combinedResults = [...formattedResult1, ...formattedResult2];
