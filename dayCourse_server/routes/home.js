c3bdc32aaddf9af6fba8fb8beb445c8811b3a91a
kimkeongeun <keongeun01@gmail.com>
2024-10-30 23:14:39 +0900

    실수고침

--- dayCourse_server/routes/home.js
+++ dayCourse_server/routes/home.js
@@ -712,7 +712,7 @@ router.post('/plan/:planId/images', upload.single('image'), async (req, res) => 
 
 
 
-app.get('/plan/moment', authenticateJWT, async (req, res) => {
+router.get('/plan/moment', authenticateJWT, async (req, res) => {
     const userId = req.user.userId;
 
     const sql = `
