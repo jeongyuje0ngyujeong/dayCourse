06122fd418a349d2ef6553c8bb5763544c4950f6
kimkeongeun <keongeun01@gmail.com>
2024-10-30 13:46:47 +0900

    group_g_4

--- dayCourse_server/routes/group.js
+++ dayCourse_server/routes/group.js
@@ -215,8 +215,12 @@ router.post('/add', authenticateJWT, async (req, res) => {
 
         const existingUserIds = new Set(existsResults.map(row => row.userId)); // 이미 존재하는 사용자 ID 세트
 
+
+
         // 멤버 추가 쿼리 실행
         console.log('멤버추가');
+        console.log(allUserData);
+        
         for (const user of allUserData) {
             if (!existingUserIds.has(user.userId)) {
                 await new Promise((resolve, reject) => {
