d1699c10c2715eef1262f5e7c4652551e160ab23
kimkeongeun <keongeun01@gmail.com>
2024-10-31 14:57:07 +0900

    웹소켓 서버 병합

--- dayCourse_server/app.js
+++ dayCourse_server/app.js
@@ -5,6 +5,9 @@ const groupRoutes = require('./routes/group.js');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const passport = require('passport');
+const http = require('http')
+
+const socketio = require('socket.io')
 
 const app = express();
 dotenv.config();
@@ -21,6 +24,17 @@ const authenticateJWT = require('./config/authenticateJWT');
 require('./config/passport')(passport);
 app.use(passport.initialize());
 
+//웹소켓 관련
+const server = http.createServer(app)
+const io = socketio(server, {
+  cors: {
+    origin: '*',  // React 클라이언트 허용
+    methods: ['GET', 'POST'],         // 허용할 메서드
+    credentials: true                 // 쿠키 사용 허용
+  }
+});
+
+
 // /home 경로에 있는 모든 라우트에 인증 미들웨어 적용
 app.use('/home', authenticateJWT, apiHome);
 
@@ -31,6 +45,58 @@ app.use("/group", groupRoutes);
 
 app.get('/', (req, res) => {
   res.send('테스트')
+})
+
+
+io.on('connection', (socket) => {
+  console.log('새로운 유저가 접속했습니다.')
+  
+  socket.on('join', ({name, room}, callback) => {
+    console.log('name:', name, 'room:', room);
+    const { error, user } = addUser({ id: socket.id, name, room })
+    if (error) callback({error : '에러가 발생했습니다.'})
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}님, 환영합니다.`,
+    })
+
+    io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      })
+
+    socket.join(user.room)
+    callback()
+  })
+
+  socket.on('sendMessage', (message, callback) => {
+    // console.log('socket.id:', socket.id);
+    const user = getUser(socket.id)
+    // console.log('getUser result:', user);
+    // console.log(typeof message, message)
+    io.to(user.room).emit('message', {
+      user: user.name,
+      text: message,
+    })
+    callback()
+  })
+
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id)
+    if (user) {
+        io.to(user.room).emit('message', {
+        user: 'admin',
+        text: `${user.name}님이 퇴장하셨습니다.`,
+        })
+        io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+        })
+    }
+    console.log('유저가 나갔습니다.')
+  })
 })
 
 
