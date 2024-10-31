df0d3911f4d86d1961c21e660d9644716b0b7074
Minkyoung00 <kina12345600@gmail.com>
2024-10-31 22:44:40 +0900

    add edit button on schedule detail page

--- dayCourse_client/client/src/pages/Calendar/Schedule.js
+++ dayCourse_client/client/src/pages/Calendar/Schedule.js
@@ -51,7 +51,7 @@ export default function Schedule(props) {
   
   const [selectedSchedules, groupedSchedules, setGroupedSchedules] = useOutletContext() || [props.selectedSchedules, props.groupedSchedules, props.setGroupedSchedules];
 
-  console.log(selectedSchedules);
+  // console.log(selectedSchedules);
 
   function updateSchedulesForDate(dateKey, planId) {
     setGroupedSchedules(prevSchedules => {
@@ -87,7 +87,7 @@ export default function Schedule(props) {
 
                   if (`${event.start_userId}` === sessionStorage.getItem('id')){
                     const result = await deleteSchedule(event.planId);
-                    console.log(result);
+                    // console.log(result);
                     if (result === 'success'){
                       updateSchedulesForDate(event.dateKey, event.planId)
                     
