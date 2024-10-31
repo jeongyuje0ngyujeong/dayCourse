77f0dac41c7bd4c0bd3de7c657375dda145d9285
Minkyoung00 <kina12345600@gmail.com>
2024-10-31 11:21:55 +0900

    modifing calendar

--- dayCourse_client/client/src/pages/Home/Home.js
+++ dayCourse_client/client/src/pages/Home/Home.js
@@ -1,13 +1,20 @@
 import styled from "styled-components";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { PageTitle, Footer } from '../../commonStyles';
-import {DayTable, GroupDatesByWeek} from '../Calendar/Calendar'
-// import Schedule from '../Calendar/Schedule'
+import {DayTable, GroupDatesByWeek} from '../Calendar/CalendarComponent'
 import { Button } from '../../Button';
 import { Outlet, Form} from "react-router-dom";
 import { getSchedules } from "../../schedules";
-// import { Link } from 'react-router-dom'; 
-// import RightSidebar from './RightSidebar'; 
+
+export async function action() {
+    // const schedule = await createSchedule();
+    // return redirect(`/schedules/create`);
+  }
+  
+export async function loader() {
+    const schedules= await getSchedules();
+    return { schedules };
+}
 
 const WeekBar = styled.div`
   display: flex;
@@ -39,75 +46,80 @@ const ScheduleContainer = styled.div `
   clear: left;
 `
 
-export async function action() {
-  // const schedule = await createSchedule();
-  // return redirect(`/schedules/create`);
-}
 
-export async function loader() {
-  const schedules= await getSchedules();
-  return { schedules };
-}
 
-// 날짜칸을 선택 -> useState 변경-> loader의 인자 -> loader의 return값을 얻어옴
 export default function Home() {
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [groupedSchedules, setGroupedSchedules] = useState([]);
+    const [selectedDate, setSelectedDate] = useState([]);
+    const selectedSchedules = groupedSchedules[selectedDate];
 
-  const [currentDate, setCurrentDate] = useState(new Date());
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth();
-  const date = currentDate.getDate();
-  
-  const today = new Date(year, month, date);
-  const startDay = new Date(currentDate);
-  startDay.setDate(today.getDate() - today.getDay());
+    useEffect(() => {
+        async function loadSchedules() {
+            try {
+                const loadSchedules = await getSchedules(null, currentDate); 
+                const grouped = loadSchedules.reduce((acc, curr) => {
+                    if (!acc[curr.dateKey]) {
+                        acc[curr.dateKey] = [];
+                    }
 
-  const endDay = new Date(startDay);
-  endDay.setDate(startDay.getDate() + 6);
+                    acc[curr.dateKey].push(curr);
+                    return acc;
+                }, {});
 
-  const st_month = String(currentDate.getMonth() + 1).padStart(2, '0');
+                setGroupedSchedules(grouped);
+            } catch (error) {
+                console.error('Error loading schedules:', error);
+            }}
+            loadSchedules(); 
+            
+    }, [currentDate]);
+    
+    console.log('groupedSchedules: ',groupedSchedules); 
 
-  const handlePrevWeek = () => {
+    const currentDay = currentDate.getDay();
+    const startDay = new Date(currentDate);
+    startDay.setDate(currentDate.getDate() - currentDay);
+    const endDay = new Date(startDay);
+    endDay.setDate(startDay.getDate() + 6);
+
+    const handlePrevWeek = () => {
+        setCurrentDate(
+          new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()-7)
+        );
+      };
+      
+    const handleNextWeek = () => {
     setCurrentDate(
-      new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()-7)
+        new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()+7)
     );
-  };
-  
-  const handleNextWeek = () => {
-    setCurrentDate(
-      new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()+7)
-    );
-  };
+    };
 
-  const [ schedules, setSchedules ] = useState([]);
+    return(
+        <>
+        <PageTitle>Home</PageTitle>
+        <WeekBar>
+            <h3>한 주 일정</h3>
+            <Form action="/main/schedules/create">
+                <Button type='submit' width='6rem' $background='#90B54C' color='white'>+ 일정추가</Button>
+            </Form>
+        </WeekBar>
+        <MonthContainer>
+            <h3>{currentDate.getFullYear()}. {String(currentDate.getMonth() + 1).padStart(2, '0')}</h3>
+            <Button onClick={() => handlePrevWeek()} $border='none'>{'<'}</Button>
+            <Button onClick={() => handleNextWeek()} $border='none'>{'>'}</Button>
+        </MonthContainer>
 
-  return (
-    <>
-    <PageTitle>Home</PageTitle>
-    <CalendarContainer>
-      <WeekBar>
-        <h3>한 주 일정</h3>
-        <Form action="/main/schedules/create">
-          <Button type='submit' width='6rem' $background='#90B54C' color='white'>+ 일정추가</Button>
-        </Form>
-      </WeekBar>
-      <MonthContainer>
-        <h3>{year}. {st_month}</h3>
-        <Button onClick={() => handlePrevWeek()} $border='none'>{'<'}</Button>
-        <Button onClick={() => handleNextWeek()} $border='none'>{'>'}</Button>
-      </MonthContainer>
-  
-      <DayTable/> 
-      {/* 주단위 달력 */}
-      <GroupDatesByWeek startDay={startDay} endDay={endDay}/>
+        <DayTable/> 
+        {/* 주단위 달력 */}
+        <GroupDatesByWeek groupedSchedules={groupedSchedules} setGroupedSchedules={setGroupedSchedules} startDay={startDay} endDay={endDay} setSelectedDate={setSelectedDate}/>
 
-      {/* 선택 날짜 일정 표시 */}
-      <ScheduleContainer>
-        <Outlet context={[schedules, setSchedules]}/>
-      </ScheduleContainer>
+        <ScheduleContainer>
+          <Outlet context={[selectedSchedules, groupedSchedules ,setGroupedSchedules]}/>
+        </ScheduleContainer>
 
-      <Footer/>
-    </CalendarContainer>
+        <Footer/>
+        </>
+    )
 
-    </>
-  )
 }
