aef38bd6cd0d9718115ddcd03a00fefefdc4e84c
Minkyoung00 <kina12345600@gmail.com>
2024-11-01 12:06:30 +0900

    commit to deploy3

--- dayCourse_client/client/src/pages/Calendar/Schedule.js
+++ dayCourse_client/client/src/pages/Calendar/Schedule.js
@@ -50,8 +50,6 @@ export default function Schedule(props) {
   
   // const loaderData = useLoaderData();
   
-  const [selectedSchedules, setGroupedSchedules] = useOutletContext() || [props.selectedSchedules, props.setGroupedSchedules];
-  // const [selectedSchedules, groupedSchedules, setGroupedSchedules] = useOutletContext() || [props.selectedSchedules, props.groupedSchedules, props.setGroupedSchedules];
   const [selectedSchedules, , setGroupedSchedules] = useOutletContext() || [props.selectedSchedules, , props.setGroupedSchedules];
   // const [selectedSchedules, groupedSchedules, setGroupedSchedules] = useOutletContext() || [props.selectedSchedules, props.groupedSchedules, props.setGroupedSchedules];
   // console.log(selectedSchedules);
