2bf1c54ebbffc037f8dd94187f9d4b86f69e333d
aehyemin <hsy13467926@gmail.com>
2024-11-04 23:31:47 +0900

    랜딩 페이지 스타일 수정

--- dayCourse_client/client/src/pages/Home/LandingPage.js
+++ dayCourse_client/client/src/pages/Home/LandingPage.js
@@ -15,23 +15,40 @@ const SelectedPlacesContainer = styled.div`
     flex-direction: column; 
 `;
 const PlaceBox = styled.div`
+    display: flex;
+    align-items: center; /* 수직 중앙 정렬 */
+    justify-content: space-between; /* 공간을 양쪽 끝에 배치 */
+    width: 35%;
     margin: 5px;
-    padding: 5px;
+    padding: 10px;
     border: 1px solid #ddd;
     border-radius: 10px;
     box-shadow: 0 4px 6px rgba(0.1, 0.1, 0.1, 0.1);
     transition: box-shadow 0.3s ease;
+
     &:hover {
         box-shadow: 0 6px 10px rgba(0.15, 0.15, 0.15, 0.15);
     }
+
+    h5 {
+        margin: 0;
+        font-size: 16px;
+        font-weight: normal;
+    }
+
+    span {
+        font-size: 14px;
+        color: #666;
+    }
 `;
 const DeleteButton = styled.button`
-    margin-top: 5px;
+    margin-left: 10px; 
     background-color: #ff4d4d;
     color: white;
     border: none;
     border-radius: 4px;
     cursor: pointer;
+    padding: 5px 10px;
 
     &:hover {
         background-color: #e60000;
