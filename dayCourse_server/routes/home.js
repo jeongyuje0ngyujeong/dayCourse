e2f3db2d32bb5ffb676ed5e1ab3eb657400c0c01
kimkeongeun <keongeun01@gmail.com>
2024-11-04 15:40:54 +0900

    ai api 변경2

--- dayCourse_server/routes/home.js
+++ dayCourse_server/routes/home.js
@@ -862,10 +862,10 @@ router.get('/plan/moment', authenticateJWT, async (req, res) => {
             console.log("분석시작")
 
             // 이미지 데이터를 분석 서비스로 전송
-            const response = await axios.post('http://13.124.135.96:5000/cluster', form, {
+            const response = await axios.post('http://13.124.135.96:5000/cluster', { images: imagesData }, {
                 headers: {
                     'Accept': 'application/json',
-                    ...form.getHeaders() // FormData의 헤더 추가
+                    'Content-Type': 'application/json',
                 },
             });
 
