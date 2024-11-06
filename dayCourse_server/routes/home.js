870708af0b762fc6cfa8158feb202716e0dc36ca
kimkeongeun <keongeun01@gmail.com>
2024-11-07 00:39:14 +0900

    locationId 저장안됨

--- dayCourse_server/routes/home.js
+++ dayCourse_server/routes/home.js
@@ -1064,7 +1064,7 @@ router.post('/plan/:enCategory/:enKeyword?', authenticateJWT, async (req, res) =
             phone: "01000000000" //필드없음
         }));
 
-        console.log("응답 : ", renamedUsers)
+        //console.log("응답 : ", renamedUsers)
 
         return res.status(200).json({ msg: 'success', place: renamedUsers });
     } else {
@@ -1130,7 +1130,7 @@ router.post('/plan/:enCategory/:enKeyword?', authenticateJWT, async (req, res) =
             phone: "01000000000" //필드없음
         }));
 
-        console.log(renamedUsers);
+        //console.log(renamedUsers);
         return res.status(200).json({ msg: 'success', place: renamedUsers });
     }
 });
