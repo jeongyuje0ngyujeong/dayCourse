d78541666b4112f5c618dd14f494ec8dcfc01132
jeongyuje0ngyujeong <jeongyuje0ngyujeong.j@gmail.com>
2024-11-07 01:49:33 +0900

    tmap test

--- dayCourse_server/routes/home.js
+++ dayCourse_server/routes/home.js
@@ -625,17 +625,17 @@ router.post('/plan/place_distance', authenticateJWT, async (req, res) => {
             "transportMode": "pedestrian"
         };
         // 요청 초과 예외처리용
-        const response = null;
+        // const response = null;
 
         // 오픈 api 요청
-        // const response = await axios.post('https://apis.openapi.sk.com/tmap/matrix?version=1',
-        //     data, {
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json',
-        //         'appKey': APP_KEY
-        //     }
-        // });
+        const response = await axios.post('https://apis.openapi.sk.com/tmap/matrix?version=1',
+            data, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'appKey': APP_KEY
+            }
+        });
 
         // 요청 초과 예외처리용
         if (response != null) {
