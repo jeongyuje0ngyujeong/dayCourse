d1900acf35d00fdda16fdfed1f2016193be2867f
aehyemin <hsy13467926@gmail.com>
2024-11-07 11:45:10 +0900

    채팅창 크기 변경

--- dayCourse_client/client/src/pages/Chat/Chat.js
+++ dayCourse_client/client/src/pages/Chat/Chat.js
@@ -17,11 +17,11 @@ const ChatContainer = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
-    height: 90%;
+    height: 85%;
 `;
 
 const ChatName = styled.div`
-    flex: 0 0 10%;
+    flex: 0 0 5%;
     display: flex;
     width: 100%;
     height: 10%;
