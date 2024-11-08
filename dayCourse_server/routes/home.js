62120fbc4d4f8d9f9e2a408a16fbc057c9e34ea9
jeongyuje0ngyujeong <jeongyuje0ngyujeong.j@gmail.com>
2024-11-08 14:06:19 +0900

    tmap api close

--- dayCourse_server/routes/home.js
+++ dayCourse_server/routes/home.js
@@ -629,17 +629,17 @@ router.post('/plan/place_distance', authenticateJWT, async (req, res) => {
             "transportMode": "pedestrian"
         };
         // 요청 초과 예외처리용
-        // const response = null;
+        const response = null;
 
-        // 오픈 api 요청
-        const response = await axios.post('https://apis.openapi.sk.com/tmap/matrix?version=1',
-            data, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'appKey': APP_KEY
-            }
-        });
+        // // 오픈 api 요청
+        // const response = await axios.post('https://apis.openapi.sk.com/tmap/matrix?version=1',
+        //     data, {
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json',
+        //         'appKey': APP_KEY
+        //     }
+        // });
 
         // 요청 초과 예외처리용
         if (response != null) {
