b1c1a43b862ae1d9b519f1ba7a7eb6171361f723
Minkyoung00 <kina12345600@gmail.com>
2024-11-12 17:44:35 +0900

    add loading animation on town recommand box & add effect on new chat

--- dayCourse_client/client/src/pages/Chat/Messages.js
+++ dayCourse_client/client/src/pages/Chat/Messages.js
@@ -1,14 +1,15 @@
 // import React from 'react';
 import styled from 'styled-components';
 import Message from "./Message";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ChatContent = styled.div`
     flex: 1;
     display: flex;
     flex-direction: column-reverse;
     width: 100%;
-    margin: 0.5rem 0;
+    margin: 2vh 0;
+    padding: 1vh;
    
     color: white;
     overflow: auto; 
@@ -20,17 +21,24 @@ const ChatContent = styled.div`
 
 
 export default function Messages({ messages, name }) {
+    
+    // const latestMessageIndex = 0;
+    const [highlightMessageIndex, setHighlightMessageIndex] = useState(0); 
+    
     useEffect(() => {
-        if (messages) {
-            console.log('전체 메세지: ', messages);
-        }
-      }, [messages]);
+        setHighlightMessageIndex(0);
 
+        const timer = setTimeout(() => {
+            setHighlightMessageIndex(null); // 2초 후 강조 제거
+          }, 500); // 2초 후 강조 제거
+    
+          return () => clearTimeout(timer); // cleanup
+      }, [messages]);
     return (
         <ChatContent>
             {messages.length>0 && messages.map((message, index) => (
                 <div key={index}>
-                    <Message user={message.user} text={message.text} name={name}/>
+                    <Message user={message.user} text={message.text} name={name} isNewMessage={index === highlightMessageIndex}/>
                 </div>
             ))}
         </ChatContent>
