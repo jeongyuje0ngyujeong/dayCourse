ef4463b4ee8b9b80e3053122a47fbc353286dc1d
Minkyoung00 <kina12345600@gmail.com>
2024-11-12 17:44:35 +0900

    add loading animation on town recommand box & add effect on new chat

--- dayCourse_client/client/src/pages/Chat/Message.js
+++ dayCourse_client/client/src/pages/Chat/Message.js
@@ -4,46 +4,94 @@ import styled from 'styled-components';
 const ChatMessage = styled.div`
     display: inline-flex;
     align-items: center;
-    height:auto; 
-    padding: 0.5rem 0.5rem;
+    padding: 1vh 1vh;
     background: #ccc;
     color: black;
-    border-radius: 5px;
+    border-radius: 2vh;
     max-width: 80%;
     word-wrap: break-word;
-    position: relative;
-    margin-left: 0;
+    font-size: 2vh;
+
+    ${'' /* animation: ${({ isNewMessage }) => isNewMessage && 'highlight 0.3s ease-out forwards'};
+    @keyframes highlight {
+        0% {
+        background-color: #ffeb3b; 
+        transform: scale(2);
+        }
+        100% {
+        background-color: #90B54C; 
+        transform: scale(1); /
+        }
+    } */}
+
+    animation: ${({ isNewMessage }) => isNewMessage && 'highlightLeft 0.3s ease-out forwards'};
+
+    @keyframes highlightLeft {
+        0% {
+      transform: translateX(-100%); 
+    }
+    100% {
+      transform: translateX(0); 
+    }
+    }
 `;
 
 const ChatMessage2 = styled.div`
     display: inline-flex;
     align-items: center;
-    height:auto; 
-    padding: 0.5rem 0.5rem;
+    padding: 1vh 1vh;
     background: #90B54C;
-    border-radius: 5px;
-    width: fit-content;
+    color: white;
+    border-radius: 2vh;
+    max-width: 70%;
     word-wrap: break-word;
-    position: relative;
-    margin-left: 0;
+    font-size: 2vh;
+
+    ${'' /* animation: ${({ isNewMessage }) => isNewMessage && 'highlight 0.3s ease-out forwards'};
+    @keyframes highlight {
+        0% {
+        background-color: #ffeb3b; 
+        transform: scale(2);
+        }
+        100% {
+        background-color: #90B54C; 
+        transform: scale(1); /
+        }
+    } */}
+
+
+    animation: ${({ isNewMessage }) => isNewMessage && 'highlightRight 0.3s ease-out forwards'};
+  
+    @keyframes highlightRight {
+        0% {
+      transform: translateX(100%);
+    }
+    100% {
+      transform: translateX(0); 
+    }
+    }
+
+    
 `;
 
+const MessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 0vh;
+`;
 
-function Message({  user, text, name  }) {
-  // console.log(text);
-  let isSentByCurrentUser = false;  
-  
-  const trimmedName = name.trim().toLowerCase();
+function Message({ user, text, name, isNewMessage }) {
+  const isSentByCurrentUser = user.trim().toLowerCase() === name.trim().toLowerCase();
 
   return (
       <MessageContainer style={{ alignItems: isSentByCurrentUser? 'flex-end' : 'flex-start', fontSize:'2vh'}}>
-          <p style={{ color: 'black' }}>{isSentByCurrentUser? name : user}</p>
+          <p style={{ color: 'black', background:'white', border:'1px solid white', borderRadius:'20vh', padding:'0 1vh ', marginBottom:'0' }}>{isSentByCurrentUser? name : user}</p>
           {isSentByCurrentUser? (
-              <ChatMessage2>
+              <ChatMessage2 isNewMessage={isNewMessage}>
                   {text}
               </ChatMessage2>
           ) : (
-              <ChatMessage>
+              <ChatMessage isNewMessage={isNewMessage}>
                   {text}
               </ChatMessage>
           )}
