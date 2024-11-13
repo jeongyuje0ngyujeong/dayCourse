bee6d76a6d1acbf9ab1b7e4c3e5a0e62c6b993b5
aehyemin <hsy13467926@gmail.com>
2024-11-13 16:05:44 +0900

    거리 정보 비활성화, 채팅사운드

--- dayCourse_client/client/src/pages/Chat/Chat.js
+++ dayCourse_client/client/src/pages/Chat/Chat.js
@@ -1,5 +1,5 @@
 // src/pages/Chat/Chat.js
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import styled from 'styled-components';
 import SocketContext from '../../SocketContext';
 import Messages from './Messages';
@@ -23,16 +23,27 @@ const ChatContainer = styled.div`
 //     align-items: center;
 // `;
 
+const chatSound = new Audio('/chatSound_copy.wav');
+
 export default function Chat({ userId, planInfo }) {
     const { messages, sendMessage } = useContext(SocketContext); // 소켓 컨텍스트에서 메시지 및 함수 가져오기
     // const [name, setName] = useState(userId);
     // const [room, setRoom] = useState(planInfo.planId);
     const [message, setMessage] = useState('');
     // const [userNames, setUserNames] = useState('');
+    const [previousMessagesCount, setPreviousMessagesCount] = useState(0); // 이전 메시지 수 상태 추가
 
     // useEffect(() => {
     //     setUserNames(users.length > 0 ? users.map((item) => item.name).join(', ') : '');
     // }, [users]);
+
+    useEffect(() => {
+        if (messages.length > previousMessagesCount) { // 새로운 메시지가 도착했는지 확인
+            chatSound.play();
+        }
+        setPreviousMessagesCount(messages.length); // 현재 메시지 수를 상태에 저장
+    }, [messages, previousMessagesCount]);
+
 
     const handleSendMessage = (event) => {
         event.preventDefault();
