7b1493c3bd62c391d0de3e2151bcb91460290c69
kimkeongeun <keongeun01@gmail.com>
2024-11-15 16:10:26 +0900

    mypage5

--- dayCourse_server/routes/mypage.js
+++ dayCourse_server/routes/mypage.js
@@ -6,24 +6,50 @@ const authenticateJWT = require('../config/authenticateJWT');
 
 // 유저 정보 조회
 router.get('/load', authenticateJWT, async (req, res) => {
-
     const userId = req.user.userId;
+    console.log("유저 정보 조회 : ",  userId)
 
     const sql = `
-        SELECT User.userName, User.userAge User.userGender, User_survey.*
+        SELECT User.userName, User.userAge, User.userGender, User_survey.*
         FROM User
         LEFT JOIN User_survey ON User.userId = User_survey.userId;
         WHERE User.userId = ?
     `;
+    
+    try {
+        db.query(sql, [userId], async (err, result) => {
+            if (err) {
+                console.error('Error fetching data:', err);
+                return res.status(500).json({ error: 'Database error' });
+            }
+            
+            console.log("결과: ",  result)
+            return res.status(201).json(result);
+        });
 
-    // const sql = `
-    //     SELECT User.*, User_survey.*
-    //     FROM User
-    //     INNER JOIN User_survey ON User.userId = User_survey.userId
-    //     WHERE User.userId = ?
+    } catch (error) {
+        console.error('error:', error);
+        return res.status(500).json({ error: 'error' });
+    }
+
+})
+
+// 유저 정보 업데이트
+router.post('/load', authenticateJWT, async (req, res) => {
+    const userId = req.user.userId;
+    console.log("유저 정보 업데이트",req.body)
+
+    const User_update = `
+      UPDATE User
+      SET userName = ?, userAge = ?, userGender = ?
+      WHERE userId = ?;
+    `;
+
+    // const User_survey_update = `
+    // UPDATE User_survey
+    // SET userName = ?, userAge = ?, userGender = ?
+    // WHERE userId = ?;
     // `;
-
-    console.log("유저 정보 조회 : ",  userId)
     
     try {
         db.query(sql, [userId], async (err, result) => {
